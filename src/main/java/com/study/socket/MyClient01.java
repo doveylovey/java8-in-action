c86c511cabd5a5da4272111a629988b0f436efeb
doveylovey <1135782208@qq.com>
2020-01-14 14:47:11 +0800

    Java Socket 示例

---
+++ src/main/java/com/study/socket/MyClient01.java
@@ -0,0 +1,62 @@
+package com.study.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * socket 客户端
+ *
+ * @author Administrator
+ */
+public class MyClient01 {
+    public static void main(String[] args) {
+        // 获取用户输入的数据
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入数据：");
+        String inputData = input.nextLine();
+
+        Socket socket = null;
+        OutputStream outputStream = null;
+        InputStream inputStream = null;
+        try {
+            // 开启一个Socket端口
+            socket = new Socket("127.0.0.1", 6666);
+            outputStream = socket.getOutputStream();
+            outputStream.write(inputData.getBytes());
+            // 获取服务端回传的数据
+            inputStream = socket.getInputStream();
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            len = inputStream.read(buffer);
+            String getData = new String(buffer, 0, len);
+            System.out.println("从服务端获取的数据：" + getData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/study/socket/MyServer01.java
@@ -0,0 +1,73 @@
+package com.study.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * socket 服务端
+ *
+ * @author Administrator
+ */
+public class MyServer01 {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            serverSocket = new ServerSocket(6666);
+            boolean flag = true;
+            while (flag) {
+                // 接收客户端的请求
+                System.out.println(">>>>>>>>>> 开始监听客户端数据 >>>>>>>>>>");
+                socket = serverSocket.accept();
+                inputStream = socket.getInputStream();
+                byte[] buffer = new byte[1024];
+                int len = -1;
+                len = inputStream.read(buffer);
+                String str = new String(buffer, 0, len);
+                System.out.println("从客户端获取的数据：" + str);
+                // 业务处理：大小写转化
+                String upperCase = str.toUpperCase();
+                // 向客户端写数据
+                outputStream = socket.getOutputStream();
+                outputStream.write(upperCase.getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
